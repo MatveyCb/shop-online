87e7b1a8c3fbf7154e3c01ad4e9f6864d9f638b4
Matei Ceban <mateiceban2010@gmail.com>
2024-07-10 11:58:57 +0100

    add comments service

--- src/script.js
+++ src/script.js
@@ -1,12 +1,12 @@
 import {
-    getData, rendercard
+    getData, rendercard, renderComments, postComment
 } from "./script/service.js"
 
 let firstslider = document.querySelector("#hits")
-
 let secondslider = document.querySelector("#discount")
-
 let tirdslider = document.querySelector("#acssesoris")
+
+let feedbackList = document.querySelector(".feedback__list")
 
 getData("products", "?popular=true").then(data => {
     console.log(firstslider)
@@ -23,10 +23,28 @@ getData("products", "?category=accessories").then(data => {
     rendercard(data, tirdslider, true)
 })
 
+getData("comments").then(comments => renderComments(comments, feedbackList))
+
 let addFeedback = document.querySelector(".addfeedback")
 
 let feedbackForm = document.querySelector(".feedback__form")
 
+let feedbackInps = document.querySelectorAll(".feedback__form input")
+
+let feedbackTxt = document.querySelector(".feedback__form textarea")
+
+feedbackForm.addEventListener("submit", (event) => {
+    event.preventDefault()
+
+    postComment({
+        author: feedbackInps[0].value,
+        title: feedbackInps[1].value,
+        text: feedbackTxt.value
+    }).then((data) => {
+        console.log(data)
+        getData("comments").then(comments => renderComments(comments, feedbackList))
+    })
+})
 addFeedback.addEventListener("click", () => {
     feedbackForm.classList.toggle("active")
 })
--- src/script/service.js
+++ src/script/service.js
@@ -1,18 +1,18 @@
 let url = "https://66573aef9f970b3b36c8790d.mockapi.io/"
 
-async function getData(param,options=""){
-    let res = await fetch(url+param+options)
+async function getData(param, options = "") {
+    let res = await fetch(url + param + options)
     let data = await res.json()
     console.log(data)
     return data
 }
 
-function rendercard(data,selector,param){
-    selector.innerHTML=""
+function rendercard(data, selector, param) {
+    selector.innerHTML = ""
     data.forEach(element => {
         console.log(element)
-        selector.innerHTML+=`
-        ${param?'<div class="swiper-slide">':""}
+        selector.innerHTML += `
+        ${param ? '<div class="swiper-slide">' : ""}
         <div class="product-card">
         <img src="${element.img}" alt="">
         <h3>
@@ -26,11 +26,51 @@ function rendercard(data,selector,param){
                 Купити
             </button>
         </div>
-    </div> ${param?'</div>':""}`
-    
+    </div> ${param ? '</div>' : ""}`
+
     });
+}
+async function postComment(newComment) {
+    let res = await fetch(url + "comments", {
+        method: `POST`,
+        headers: { 'content-type': 'application/json' },
+        body: JSON.stringify(newComment)
+    })
+    let data = await res.json()
+    return data
+}
+
+function convertTimestamp(timestamp) {
+    let data = new Date(timestamp * 1000);
+    let day = String(data.getDate()).padStart(2, `0`);
+    let month = String(data.getMonth() + 1).padStart(2, `0`);
+    let year = data.getFullYear();
+
+    return `${day}.${month}.${year}`
+}
+
+function renderComments(comments, selector) {
+    selector.innerHTML = ""
+    comments.forEach(comment => {
+        selector.innerHTML += `<div class="feedback__list-item">
+        <h3>
+            ${comment.title}
+        </h3>
+        <p class="feedback__dsk">
+            ${comment.text}
+        </p>
+        <div class="row">
+            <p>
+                ${comment.author}
+            </p>
+            <p>
+                ${convertTimestamp(+comment.date)}
+            </p>
+        </div>
+    </div>`
+    })
 }
 
 export {
-    getData,rendercard
+    getData, rendercard, renderComments, postComment
 }
